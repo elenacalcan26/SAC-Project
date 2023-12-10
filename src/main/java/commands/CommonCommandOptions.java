adc094eaf0341bbdc76f976fdae42338e7ba02ed
elenacalcan26 <elena.calcan26@gmail.com>
2023-12-10 13:49:30 +0200

    add simple items to user recommendation functionality

--- src/main/java/commands/CommonCommandOptions.java
+++ src/main/java/commands/CommonCommandOptions.java
@@ -1,2 +1,10 @@
-package commands;public class CommonCommandArguments {
+package commands;
+
+import com.github.rvesse.airline.annotations.Option;
+import com.github.rvesse.airline.annotations.restrictions.Required;
+
+public class CommonCommandOptions {
+  @Required
+  @Option(name = "--userId", description = "The user id registered in Recombee")
+  String userId;
 }
--- src/main/java/commands/ItemsRecommendationCommand.java
+++ src/main/java/commands/ItemsRecommendationCommand.java
@@ -1,2 +1,29 @@
-package commands;public class ItemsRecommendetionCommand {
+package commands;
+
+import com.github.rvesse.airline.annotations.Command;
+import com.recombee.api_client.exceptions.ApiException;
+import recombee.RecombeeClientWrapper;
+
+import javax.inject.Inject;
+
+@Command(
+    name = "recommend",
+    description = "Recommend movies for the user with the given userId as parameter"
+)
+public class ItemsRecommendationCommand implements Runnable {
+
+  @Inject
+  private CommonCommandOptions commonOptions;
+
+  private RecombeeClientWrapper clientWrapper = RecombeeClientWrapper.getInstance();
+
+  @Override
+  public void run() {
+    String userId = commonOptions.userId;
+    try {
+      clientWrapper.recommendMoviesToUser(userId);
+    } catch (ApiException e) {
+      throw new RuntimeException(e);
+    }
+  }
 }
